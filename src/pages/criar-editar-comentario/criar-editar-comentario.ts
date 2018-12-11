82a4c5fec37724e5128c82fee5343a2b4cb116ab
ivancarvalhofilho <ivancarvalhofilho@github.com>
2018-12-11 10:05:16 -0200

    corrigido editar jogog

--- src/pages/criar-editar-comentario/criar-editar-comentario.ts
+++ src/pages/criar-editar-comentario/criar-editar-comentario.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams,ToastController } from 'ionic-angular';
-import { RestApiProvider } from './../../providers/rest-api/rest-api';
-import { Session } from './../../providers/share/session';
+import { Component } from "@angular/core";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  ToastController
+} from "ionic-angular";
+import { RestApiProvider } from "./../../providers/rest-api/rest-api";
+import { Session } from "./../../providers/share/session";
 
 /**
  * Generated class for the CriarEditarComentarioPage page.
@@ -12,71 +17,108 @@ import { Session } from './../../providers/share/session';
 
 @IonicPage()
 @Component({
-  selector: 'page-criar-editar-comentario',
-  templateUrl: 'criar-editar-comentario.html',
+  selector: "page-criar-editar-comentario",
+  templateUrl: "criar-editar-comentario.html"
 })
 export class CriarEditarComentarioPage {
-    private idJogo: string;
-    private userId: string;
-    private comentarioUsuarioId: string;
-    private nomeUsuario: string;
-    private nota: number;
-    private mensagem :string;
-  
-    set _nota(nota: number){
+  private idJogo: string;
+  private userId: string;
+  private comentarioUsuarioId: string;
+  private nomeUsuario: string;
+  private nota: number;
+  private mensagem: string;
+
+  set _nota(nota: number) {
     this.nota = nota;
-    }
-
-    set _mensagem(mensagem :string){
-      this.mensagem = mensagem;
-    }
-
-    constructor(public session: Session, public navCtrl: NavController, public navParams: NavParams, private api: RestApiProvider, private toastCtrl: ToastController) {
-        this.comentarioUsuarioId = this.navParams.get("comentarioUsuarioId");
-        this.userId = session.getId
-        this.idJogo = this.navParams.get("idJogo");
-        this.nomeUsuario = session.getUserName;
-    }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CriarEditarComentarioPage');
   }
 
-    cadastrarComentario() {
-      this.api.criarComentario(this.idJogo,this.userId,this.nomeUsuario,this.nota,this.mensagem).subscribe(
-        dados => {
-          if(dados == null){
-            this.toastCtrl.create({
+  set _mensagem(mensagem: string) {
+    this.mensagem = mensagem;
+  }
+
+  constructor(
+    public session: Session,
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private api: RestApiProvider,
+    private toastCtrl: ToastController
+  ) {
+    this.comentarioUsuarioId = this.navParams.get("comentarioUsuarioId");
+    this.userId = session.getId;
+    this.idJogo = this.navParams.get("idJogo");
+    this.nomeUsuario = session.getUserName;
+  }
+
+  ionViewDidLoad() {
+    console.log("ionViewDidLoad CriarEditarComentarioPage");
+  }
+
+  cadastrarComentario() {
+    this.api
+      .criarComentario(
+        this.idJogo,
+        this.userId,
+        this.nomeUsuario,
+        this.nota,
+        this.mensagem
+      )
+      .subscribe(dados => {
+        if (dados == null) {
+          this.toastCtrl
+            .create({
               duration: 3000,
               message: "Erro ao criar comentario!"
-            }).present();
-          }
-          else{
-            this.toastCtrl.create({
+            })
+            .present();
+        } else {
+          this.toastCtrl
+            .create({
               duration: 3000,
               message: "Comentario criado com sucesso!"
-            }).present();
-            this.navCtrl.pop({})
-          }
-        });     
-    }
+            })
+            .present();
+          this.navCtrl.pop({});
+        }
+      });
+  }
 
-    editarComentario() {
-      this.api.editarComentario(this.idJogo,this.userId,this.nomeUsuario,this.nota,this.mensagem).subscribe(
-        dados => {
-          if(dados == null){
-            this.toastCtrl.create({
-              duration: 3000,
-              message: "Erro ao editar comentario!"
-            }).present();
-          }
-          else{
-            this.toastCtrl.create({
-              duration: 3000,
-              message: "Comentario editado com sucesso!"
-            }).present();
-            this.navCtrl.pop()
-          }
-        });     
-    }
+  editarComentario() {
+    this.deletarComentario();
+    this.cadastrarComentario();
+    // this.api.editarComentario(this.idJogo, this.userId, this.nomeUsuario, this.nota, this.mensagem).subscribe(
+    //     dados => {
+    //         if (dados == null) {
+    //             this.toastCtrl.create({
+    //                 duration: 3000,
+    //                 message: "Erro ao editar comentario!"
+    //             }).present();
+    //         } else {
+    //             this.toastCtrl.create({
+    //                 duration: 3000,
+    //                 message: "Comentario editado com sucesso!"
+    //             }).present();
+    //             this.navCtrl.pop()
+    //         }
+    //     });
+  }
+
+  deletarComentario() {
+    this.api.deletarComentario(this.idJogo, this.userId).subscribe(dados => {
+      if (dados == null) {
+        this.toastCtrl
+          .create({
+            duration: 3000,
+            message: "Erro ao deletar comentario!"
+          })
+          .present();
+      } else {
+        this.toastCtrl
+          .create({
+            duration: 3000,
+            message: "Com1entario deletado com sucesso!"
+          })
+          .present();
+      }
+    });
+  }
 }
--- src/pages/detalhe-jogo/detalhe-jogo.ts
+++ src/pages/detalhe-jogo/detalhe-jogo.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams , ToastController} from 'ionic-angular';
-import { Jogo } from '../../model/jogo'; 
-import { RestApiProvider } from '../../providers/rest-api/rest-api';
-import { Session } from '../../providers/share/session';
-import { DetailsPage } from '../details/details';
-import { CriarEditarComentarioPage } from '../criar-editar-comentario/criar-editar-comentario';
+import {
+  Component
+} from '@angular/core';
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  ToastController
+} from 'ionic-angular';
+import {
+  Jogo
+} from '../../model/jogo';
+import {
+  RestApiProvider
+} from '../../providers/rest-api/rest-api';
+import {
+  Session
+} from '../../providers/share/session';
+import {
+  DetailsPage
+} from '../details/details';
+import {
+  CriarEditarComentarioPage
+} from '../criar-editar-comentario/criar-editar-comentario';
 
 /**
  * Generated class for the DetalheJogoPage page.
@@ -21,81 +38,83 @@ import { CriarEditarComentarioPage } from '../criar-editar-comentario/criar-edit
 export class DetalheJogoPage {
 
 
-    private jogoId: string;
-    private nomeUsuario: string;
-    private userId: string;
-    private comentarioUsuarioId: string = null;
-    private jogo: Jogo;
-    private _possuiComentario: boolean = false;
+  private jogoId: string;
+  private nomeUsuario: string;
+  private userId: string;
+  private comentarioUsuarioId: string = null;
+  private jogo: Jogo;
+  private _possuiComentario: boolean = false;
 
-    constructor(public session : Session, public navCtrl: NavController, private api: RestApiProvider, public navParams: NavParams, private toastCtrl : ToastController) {
-        this.jogoId = this.navParams.get("jogoId");
-        this.userId = session.getId;
-        this.nomeUsuario = session.getUserName;
-        this.jogo = new Jogo();
-    }
+  constructor(public session: Session, public navCtrl: NavController, private api: RestApiProvider, public navParams: NavParams, private toastCtrl: ToastController) {
+    this.jogoId = this.navParams.get("jogoId");
+    this.userId = session.getId;
+    this.nomeUsuario = session.getUserName;
+    this.jogo = new Jogo();
+  }
 
-    ionViewDidLoad() {
-        console.log('ionViewDidLoad DetalheJogoPage');
-    }
-    
-    ionViewDidEnter() {
-        this.api.obterJogo(this.jogoId).subscribe(
-            item => this.jogo = Jogo.copia(item),
-            null,
-            () => this.jogo._comentarios.forEach(element => {
-                if (element._autor == this.nomeUsuario){
-                    this.comentarioUsuarioId = element._id;
-                    this.possuiComentario = true; 
-                    return 0;
-                }
-            })
-        );
-    }
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DetalheJogoPage');
+  }
 
-    irParaCriarComentario () {
-        this.navCtrl.push(CriarEditarComentarioPage,{
-            userId: this.userId, comentarioUsuarioId: this.comentarioUsuarioId, idJogo: this.jogoId, nomeUsuario: this.nomeUsuario
-        });
-    }
-    
-    irParaVerComentarios(id: string) {
-        this.navCtrl.push(DetailsPage, {
-            jogoId: id 
-        });
-    }
+  ionViewDidEnter() {
+    this.api.obterJogo(this.jogoId).subscribe(
+      item => this.jogo = Jogo.copia(item),
+      null,
+      () => this.jogo._comentarios.forEach(element => {
+        if (element._autor == this.nomeUsuario) {
+          this.comentarioUsuarioId = element._id;
+          this.possuiComentario = true;
+          return 0;
+        }
+      })
+    );
+  }
 
-    set _jogo(jogo: Jogo) {
-        this.jogo = jogo;
-    }
+  irParaCriarComentario() {
+    this.navCtrl.push(CriarEditarComentarioPage, {
+      userId: this.userId,
+      comentarioUsuarioId: this.comentarioUsuarioId,
+      idJogo: this.jogoId,
+      nomeUsuario: this.nomeUsuario
+    });
+  }
 
-    get _jogo() {
-        return this.jogo;
-    }
+  irParaVerComentarios(id: string) {
+    this.navCtrl.push(DetailsPage, {
+      jogoId: id
+    });
+  }
 
-    get possuiComentario(): boolean {
-        return this._possuiComentario;
-    }
-    set possuiComentario(value: boolean) {
-        this._possuiComentario = value;
-    }
+  set _jogo(jogo: Jogo) {
+    this.jogo = jogo;
+  }
 
-    deletarComentario() {
-        this.api.deletarComentario(this.jogoId,this.userId).subscribe(
-          dados => {
-            if(dados == null){
-              this.toastCtrl.create({
-                duration: 3000,
-                message: "Erro ao deletar comentario!"
-              }).present();
-            }
-            else{
-              this.toastCtrl.create({
-                duration: 3000,
-                message: "Comentario deletado com sucesso!"
-              }).present();
-              this.navCtrl.pop()
-            }
-          });     
-      }
+  get _jogo() {
+    return this.jogo;
+  }
+
+  get possuiComentario(): boolean {
+    return this._possuiComentario;
+  }
+  set possuiComentario(value: boolean) {
+    this._possuiComentario = value;
+  }
+
+  deletarComentario() {
+    this.api.deletarComentario(this.jogoId, this.userId).subscribe(
+      dados => {
+        if (dados == null) {
+          this.toastCtrl.create({
+            duration: 3000,
+            message: "Erro ao deletar comentario!"
+          }).present();
+        } else {
+          this.toastCtrl.create({
+            duration: 3000,
+            message: "Comentario deletado com sucesso!"
+          }).present();
+          this.navCtrl.pop()
+        }
+      });
+  }
 }
